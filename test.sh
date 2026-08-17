d82668c08d8778718f082666b510c892c238ffb2
Maxim Satanov <satanov201354@gmail.com>
2026-08-17 23:55:31 +0500

    feat: AVX2 SIMD mod add op impl

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# if find any error - stop the script
+set -e
 
 # Wall for recommended set of warnings
 # Wextra for some additional warnings about POTENTIAL errors
-gcc -Wall -Wextra -std=c11 \
+# mavx2 for avx2 intrinsics
+gcc -Wall -Wextra -mavx2 -std=c11 \
     tests/test_modular.c \
     src/backend/scalar/modular.c \
+    src/backend/avx2/simd_modular.c \
     -o test_modular
 
 ./test_modular
